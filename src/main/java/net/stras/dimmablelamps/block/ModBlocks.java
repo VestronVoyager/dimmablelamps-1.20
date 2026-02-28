07657c706900740d2cc39920d6ead11bb847f161
Vestron <100133819+VestronVoyager@users.noreply.github.com>
2026-02-28 22:26:59 +0100

    Refactor LAMP_BLOCK registration and remove tab parameter

--- src/main/java/net/stras/dimmablelamps/block/ModBlocks.java
+++ src/main/java/net/stras/dimmablelamps/block/ModBlocks.java
@@ -1,41 +1,39 @@
 package net.stras.dimmablelamps.block;
 
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.stras.dimmablelamps.DimmableLamps;
 import net.stras.dimmablelamps.block.custom.LampBlock;
-import net.stras.dimmablelamps.item.ModCreativeModeTab;
 import net.stras.dimmablelamps.item.ModItems;
 
 import java.util.function.Supplier;
 
 public class ModBlocks {
+
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, DimmableLamps.MOD_ID);
 
-    public static final RegistryObject<Block> LAMP_BLOCK = registerBlock("lamp_block",
-            () -> new LampBlock(BlockBehaviour.Properties.of(Material.GLASS)
-                    .strength(1f).requiresCorrectToolForDrops().lightLevel(state -> state.getValue(LampBlock.LIT))), ModCreativeModeTab.DIMMABLE_LAMPS);
+    public static final RegistryObject<Block> LAMP_BLOCK = registerBlock(
+            "lamp_block",
+            () -> new LampBlock(BlockBehaviour.Properties.of()
+                    .strength(1f)
+                    .requiresCorrectToolForDrops()
+                    .lightLevel(state -> state.getValue(LampBlock.LIT)))
+    );
 
-
-    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
-        RegistryObject<T> toReturn = BLOCKS.register(name,block);
-        registerBlockItem(name,toReturn, tab);
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
         return toReturn;
     }
 
-    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab){
-        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
-    }
-    public static void register(IEventBus eventBus) {
-        BLOCKS.register(eventBus);
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return ModItems.ITEMS.register(name,
+                () -> new BlockItem(block.get(), new Item.Properties()));
     }
 }
