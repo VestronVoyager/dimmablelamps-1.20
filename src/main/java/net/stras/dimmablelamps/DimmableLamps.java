b63e4afc5acd7258e3993eaa7cc22c2b85e02ea5
Vestron <100133819+VestronVoyager@users.noreply.github.com>
2026-02-28 22:27:23 +0100

    Refactor DimmableLamps class and registration methods

--- src/main/java/net/stras/dimmablelamps/DimmableLamps.java
+++ src/main/java/net/stras/dimmablelamps/DimmableLamps.java
@@ -1,47 +1,34 @@
 package net.stras.dimmablelamps;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.stras.dimmablelamps.item.ModItems;
 import net.stras.dimmablelamps.block.ModBlocks;
+import net.stras.dimmablelamps.item.ModCreativeModeTab;
+import net.stras.dimmablelamps.item.ModItems;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(DimmableLamps.MOD_ID)
-public class DimmableLamps
-{
-    // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "dimmablelamps";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
-    public DimmableLamps()
-    {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+public class DimmableLamps {
 
-        ModItems.register(modEventBus);
-        ModBlocks.register(modEventBus);
+    public static final String MOD_ID = "dimmablelamps";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public DimmableLamps() {
+
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.ITEMS.register(modEventBus);
+        ModBlocks.BLOCKS.register(modEventBus);
+        ModCreativeModeTab.CREATIVE_MODE_TABS.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-    }
-
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-        }
     }
 }
