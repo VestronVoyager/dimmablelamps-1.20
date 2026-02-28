320ca7f0803d07acd20007e20f02dae2e593574e
Vestron <100133819+VestronVoyager@users.noreply.github.com>
2026-02-28 22:27:50 +0100

    Remove unused imports and register method

--- src/main/java/net/stras/dimmablelamps/item/ModItems.java
+++ src/main/java/net/stras/dimmablelamps/item/ModItems.java
@@ -1,23 +1,16 @@
 package net.stras.dimmablelamps.item;
 
 import net.minecraft.world.item.Item;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.stras.dimmablelamps.DimmableLamps;
 
 public class ModItems {
+
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, DimmableLamps.MOD_ID);
 
-
     public static final RegistryObject<Item> WRENCH = ITEMS.register("wrench",
-            () -> new Item(new Item.Properties().stacksTo(1).tab(ModCreativeModeTab.DIMMABLE_LAMPS)));
-
-    public static void register(IEventBus eventBus){
-        ITEMS.register(eventBus);
-    }
-
-
+            () -> new Item(new Item.Properties().stacksTo(1)));
 }
