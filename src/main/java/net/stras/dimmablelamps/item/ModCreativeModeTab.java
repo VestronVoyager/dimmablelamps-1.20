a33424adb8b9b0d704afc18a1146a6b8bf6c7cc1
Vestron <100133819+VestronVoyager@users.noreply.github.com>
2026-02-28 22:28:08 +0100

    Refactor ModCreativeModeTab to use DeferredRegister

--- src/main/java/net/stras/dimmablelamps/item/ModCreativeModeTab.java
+++ src/main/java/net/stras/dimmablelamps/item/ModCreativeModeTab.java
@@ -1,14 +1,28 @@
 package net.stras.dimmablelamps.item;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.stras.dimmablelamps.DimmableLamps;
 import net.stras.dimmablelamps.block.ModBlocks;
 
 public class ModCreativeModeTab {
-    public static final CreativeModeTab DIMMABLE_LAMPS = new CreativeModeTab("dimmablelamps") {
-        @Override
-        public ItemStack makeIcon() {
-            return new ItemStack(ModBlocks.LAMP_BLOCK.get());
-        }
-    };
+
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, DimmableLamps.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> DIMMABLE_LAMPS =
+            CREATIVE_MODE_TABS.register("dimmablelamps", () ->
+                    CreativeModeTab.builder()
+                            .title(Component.translatable("itemGroup.dimmablelamps"))
+                            .icon(() -> new ItemStack(ModBlocks.LAMP_BLOCK.get()))
+                            .displayItems((parameters, output) -> {
+                                output.accept(ModBlocks.LAMP_BLOCK.get());
+                                output.accept(ModItems.WRENCH.get());
+                            })
+                            .build()
+            );
 }
