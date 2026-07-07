52a7eca95dc2a81a7bd12a45b4057ce62b939096
Vestron <100133819+VestronVoyager@users.noreply.github.com>
2026-07-07 21:45:36 +0200

    Update ModBlocks.java

--- src/main/java/net/stras/dimmablelamps/block/ModBlocks.java
+++ src/main/java/net/stras/dimmablelamps/block/ModBlocks.java
@@ -1,65 +1,34 @@
-package net.stras.dimmablelamps.block.custom;
+package net.stras.dimmablelamps;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
-import net.minecraft.world.phys.BlockHitResult;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.stras.dimmablelamps.block.ModBlocks;
+import net.stras.dimmablelamps.item.ModCreativeModeTab;
 import net.stras.dimmablelamps.item.ModItems;
+import org.slf4j.Logger;
 
-import javax.annotation.Nullable;
+@Mod(DimmableLamps.MOD_ID)
+public class DimmableLamps {
 
-public class LampBlock extends Block {
+    public static final String MOD_ID = "dimmablelamps";
+    private static final Logger LOGGER = LogUtils.getLogger();
 
-    public static final IntegerProperty LIT = IntegerProperty.create("lit", 0, 15);
-    public static final BooleanProperty MANUAL = BooleanProperty.create("manual");
+    public DimmableLamps() {
 
-    public LampBlock(Properties properties) {
-        super(properties);
-        this.registerDefaultState(this.stateDefinition.any().setValue(LIT, 0).setValue(MANUAL, false));
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.ITEMS.register(modEventBus);
+        ModBlocks.BLOCKS.register(modEventBus);
+        ModCreativeModeTab.CREATIVE_MODE_TABS.register(modEventBus);
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
-    @Override
-    public void neighborChanged(BlockState state, Level level, BlockPos pos, Block block, BlockPos fromPos, boolean isMoving) {
-        if (!level.isClientSide) {
-            if (!state.getValue(MANUAL)) {
-                int power = level.getBestNeighborSignal(pos);
-                level.setBlock(pos, state.setValue(LIT, power), 3);
-            }
-        }
-    }
-
-    @Nullable
-    @Override
-    public BlockState getStateForPlacement(BlockPlaceContext context) {
-        int power = context.getLevel().getBestNeighborSignal(context.getClickedPos());
-        return this.defaultBlockState().setValue(LIT, power).setValue(MANUAL, false);
-    }
-
-    @Override
-    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
-        if (!level.isClientSide && hand == InteractionHand.MAIN_HAND && player.getItemInHand(hand).is(ModItems.WRENCH.get())) {
-            boolean currentManual = state.getValue(MANUAL);
-            if (!currentManual) {
-                level.setBlock(pos, state.setValue(MANUAL, true).setValue(LIT, 15), 3);
-            } else {
-                int power = level.getBestNeighborSignal(pos);
-                level.setBlock(pos, state.setValue(MANUAL, false).setValue(LIT, power), 3);
-            }
-            return InteractionResult.SUCCESS;
-        }
-        return InteractionResult.PASS;
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(LIT, MANUAL);
+    private void commonSetup(final FMLCommonSetupEvent event) {
     }
 }
