ead5aedcaad091920ef215fb71c0b1d9aa3de404
Vestron <100133819+VestronVoyager@users.noreply.github.com>
2026-07-07 21:49:33 +0200

    Update LampBlock.java

--- src/main/java/net/stras/dimmablelamps/block/custom/LampBlock.java
+++ src/main/java/net/stras/dimmablelamps/block/custom/LampBlock.java
@@ -1,34 +1,65 @@
-package net.stras.dimmablelamps;
+package net.stras.dimmablelamps.block.custom;
 
-import com.mojang.logging.LogUtils;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.stras.dimmablelamps.block.ModBlocks;
-import net.stras.dimmablelamps.item.ModCreativeModeTab;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.BlockHitResult;
 import net.stras.dimmablelamps.item.ModItems;
-import org.slf4j.Logger;
 
-@Mod(DimmableLamps.MOD_ID)
-public class DimmableLamps {
+import javax.annotation.Nullable;
 
-    public static final String MOD_ID = "dimmablelamps";
-    private static final Logger LOGGER = LogUtils.getLogger();
+public class LampBlock extends Block {
 
-    public DimmableLamps() {
+    public static final IntegerProperty LIT = IntegerProperty.create("lit", 0, 15);
 
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        ModItems.ITEMS.register(modEventBus);
-        ModBlocks.BLOCKS.register(modEventBus);
-        ModCreativeModeTab.CREATIVE_MODE_TABS.register(modEventBus);
-
-        modEventBus.addListener(this::commonSetup);
-
-        MinecraftForge.EVENT_BUS.register(this);
+    public LampBlock(Properties properties) {
+        super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(LIT, 0));
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
+    @Override
+    public void neighborChanged(BlockState state, Level level, BlockPos pos,
+                                Block block, BlockPos fromPos, boolean isMoving) {
+
+        if (!level.isClientSide) {
+            int power = level.getBestNeighborSignal(pos);
+            level.setBlock(pos, state.setValue(LIT, power), 3);
+        }
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        int power = context.getLevel().getBestNeighborSignal(context.getClickedPos());
+        return this.defaultBlockState().setValue(LIT, power);
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos,
+                                 Player player, InteractionHand hand, BlockHitResult result) {
+
+        if (!level.isClientSide && hand == InteractionHand.MAIN_HAND) {
+            ItemStack stack = player.getItemInHand(hand);
+
+            if (stack.is(ModItems.WRENCH.get())) {
+                level.setBlock(pos, state.cycle(LIT), 3);
+                return InteractionResult.SUCCESS;
+            }
+        }
+
+        return InteractionResult.PASS;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(LIT);
     }
 }
