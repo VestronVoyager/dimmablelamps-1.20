325d000c51ebe8038f034f52fb4fa68b5ecaf764
Vestron <100133819+VestronVoyager@users.noreply.github.com>
2026-07-07 20:42:19 +0200

    Update ModBlocks.java

--- src/main/java/net/stras/dimmablelamps/block/ModBlocks.java
+++ src/main/java/net/stras/dimmablelamps/block/ModBlocks.java
@@ -1,39 +1,65 @@
-package net.stras.dimmablelamps.block;
+package net.stras.dimmablelamps.block.custom;
 
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import net.stras.dimmablelamps.DimmableLamps;
-import net.stras.dimmablelamps.block.custom.LampBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.BlockHitResult;
 import net.stras.dimmablelamps.item.ModItems;
 
-import java.util.function.Supplier;
+import javax.annotation.Nullable;
 
-public class ModBlocks {
+public class LampBlock extends Block {
 
-    public static final DeferredRegister<Block> BLOCKS =
-            DeferredRegister.create(ForgeRegistries.BLOCKS, DimmableLamps.MOD_ID);
+    public static final IntegerProperty LIT = IntegerProperty.create("lit", 0, 15);
+    public static final BooleanProperty MANUAL = BooleanProperty.create("manual");
 
-    public static final RegistryObject<Block> LAMP_BLOCK = registerBlock(
-            "lamp_block",
-            () -> new LampBlock(BlockBehaviour.Properties.of()
-                    .strength(1f)
-                    .requiresCorrectToolForDrops()
-                    .lightLevel(state -> state.getValue(LampBlock.LIT)))
-    );
-
-    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
-        RegistryObject<T> toReturn = BLOCKS.register(name, block);
-        registerBlockItem(name, toReturn);
-        return toReturn;
+    public LampBlock(Properties properties) {
+        super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(LIT, 0).setValue(MANUAL, false));
     }
 
-    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
-        return ModItems.ITEMS.register(name,
-                () -> new BlockItem(block.get(), new Item.Properties()));
+    @Override
+    public void neighborChanged(BlockState state, Level level, BlockPos pos, Block block, BlockPos fromPos, boolean isMoving) {
+        if (!level.isClientSide) {
+            if (!state.getValue(MANUAL)) {
+                int power = level.getBestNeighborSignal(pos);
+                level.setBlock(pos, state.setValue(LIT, power), 3);
+            }
+        }
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        int power = context.getLevel().getBestNeighborSignal(context.getClickedPos());
+        return this.defaultBlockState().setValue(LIT, power).setValue(MANUAL, false);
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
+        if (!level.isClientSide && hand == InteractionHand.MAIN_HAND && player.getItemInHand(hand).is(ModItems.WRENCH.get())) {
+            boolean currentManual = state.getValue(MANUAL);
+            if (!currentManual) {
+                level.setBlock(pos, state.setValue(MANUAL, true).setValue(LIT, 15), 3);
+            } else {
+                int power = level.getBestNeighborSignal(pos);
+                level.setBlock(pos, state.setValue(MANUAL, false).setValue(LIT, power), 3);
+            }
+            return InteractionResult.SUCCESS;
+        }
+        return InteractionResult.PASS;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(LIT, MANUAL);
     }
 }
